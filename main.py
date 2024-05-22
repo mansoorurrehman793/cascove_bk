2c19bd84016bacc5ae5d0cfd5440e95139ee106c
muhammadumarjaved44 <muhammadumarjaved44@gmail.com>
2024-05-23 01:38:48 +0500

    adding debug code

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from typing import Union
+
 from fastapi import FastAPI, Body
 from fastapi import Body, FastAPI
 from pydantic import BaseModel, Field
 from typing_extensions import Annotated
 import requests
+import uvicorn
 
 
 app = FastAPI()
@@ -12,3 +15,5 @@ app = FastAPI()
 def read():
     return {"Hello": "World"}
 
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
